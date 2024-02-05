97e9807372c9019730086381461e45e4e0793755
mv735065 <vamshireddy203541@gmail.com>
2024-02-05 22:17:16 +0530

    3rd

--- src/main/java/com/driver/models/User.java
+++ src/main/java/com/driver/models/User.java
@@ -31,4 +31,54 @@ public  class User {
     @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
     List<Blog> blogList=new ArrayList<>();
 
+    public User() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Blog> getBlogList() {
+        return blogList;
+    }
+
+    public void setBlogList(List<Blog> blogList) {
+        this.blogList = blogList;
+    }
 }
